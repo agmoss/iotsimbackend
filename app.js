34737a323bd9eea3dbcea283bba1cb51d0935d64
Andrew Moss <agordonmoss@gmail.com>
2019-09-01 13:37:32 -0600

    Adding url params

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/devices', iotDeviceRouter);
+app.use('/api/devices', iotDeviceRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
